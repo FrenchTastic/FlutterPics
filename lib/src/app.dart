a4200985e4af9bc00acadc3c802af903e61244f8
Romain Pardon <romainpardon@gmail.com>
2018-09-18 23:00:49 +0200

    First commit

---
+++ lib/src/app.dart
@@ -0,0 +1,8 @@
+// Import Flutter Library
+import 'package:flutter/material.dart';
+
+// Create a class that will be our custom widget
+// This class must extend he 'StatelessWidget' base class
+
+// Must define  'build' method that returns
+// the widget that *this* widget ill show
